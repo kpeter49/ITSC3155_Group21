c1ae69e1ca1cb79f3c3a4ac57e27bb4bac56d0e0
Karl Peterson <kpeter49@uncc.edu>
2021-11-11 08:32:20 -0500

    Updated app.py and css to use new macros

--- app.py
+++ app.py
@@ -1,12 +1,15 @@
 # imports
 import os                 # os is used to get environment variables IP & PORT
 from flask import Flask   # Flask is the web app that we will customize
+from flask import render_template
+from flask import request
+from flask import redirect, url_for 
 
 app = Flask(__name__)     # create an app
 
 @app.route('/')
 def index():
-    return 'Hello World!'
+    return render_template('index.html')
 
 
 app.run(host=os.getenv('IP', '127.0.0.1'),port=int(os.getenv('PORT', 5000)),debug=True)
