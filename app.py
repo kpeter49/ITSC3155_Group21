25590b3a94813267b18e93ec505af647720976b6
cfrunzi <89851167+cfrunzi@users.noreply.github.com>
2021-11-11 09:14:05 -0500

    added a render to the index page

--- app.py
+++ app.py
@@ -1,15 +1,17 @@
 # imports
-import os                 # os is used to get environment variables IP & PORT
-from flask import Flask   # Flask is the web app that we will customize
+import os  # os is used to get environment variables IP & PORT
+from flask import Flask  # Flask is the web app that we will customize
 from flask import render_template
 from flask import request
-from flask import redirect, url_for 
+from flask import redirect, url_for
 
-app = Flask(__name__)     # create an app
+app = Flask(__name__)  # create an app
 
+
+# pulls up a template for the list page
 @app.route('/')
 def index():
     return render_template('index.html')
 
 
-app.run(host=os.getenv('IP', '127.0.0.1'),port=int(os.getenv('PORT', 5000)),debug=True)
+app.run(host=os.getenv('IP', '127.0.0.1'), port=int(os.getenv('PORT', 5000)), debug=True)
