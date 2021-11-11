afa3311d12184414ecf274ef459763cf88e04b55
cfrunzi <89851167+cfrunzi@users.noreply.github.com>
2021-11-11 08:57:28 -0500

    added the homepage to the application

--- app.py
+++ app.py
@@ -1,15 +1,16 @@
 # imports
-import os                 # os is used to get environment variables IP & PORT
-from flask import Flask   # Flask is the web app that we will customize
+import os  # os is used to get environment variables IP & PORT
+from flask import Flask  # Flask is the web app that we will customize
 from flask import render_template
 from flask import request
-from flask import redirect, url_for 
+from flask import redirect, url_for
 
-app = Flask(__name__)     # create an app
+app = Flask(__name__)  # create an app
+
 
 @app.route('/')
 def index():
     return render_template('index.html')
 
 
-app.run(host=os.getenv('IP', '127.0.0.1'),port=int(os.getenv('PORT', 5000)),debug=True)
+app.run(host=os.getenv('IP', '127.0.0.1'), port=int(os.getenv('PORT', 5000)), debug=True)
