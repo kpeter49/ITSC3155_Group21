5b6fd7b82275118e7f7c3d05e5451ce777c016be
Drew <dmoor121@uncc.edu>
2021-11-11 20:26:36 -0500

    implemented delete method

--- app.py
+++ app.py
@@ -1,27 +1,91 @@
 # imports
-import os                 # os is used to get environment variables IP & PORT
-from flask import Flask, redirect, url_for   # Flask is the web app that we will customize
+import os  # os is used to get environment variables IP & PORT
+from flask import Flask, redirect, url_for  # Flask is the web app that we will customize
 from flask import render_template
 from flask import request
 from database import db
 from models import Post as Post
 from models import User as User
+from datetime import date
 
-app = Flask(__name__)     # create an app
+app = Flask(__name__)  # create an app
+
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///class_forum_app.db'
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+db.init_app(app)
+
+# Setup models
+with app.app_context():
+    db.create_all()  # run under the app context
 
 
-@app.route('/')
+@app.route('/home')
+def home():
+    return render_template('home.html')
+
+
+# code for index function
+@app.route('/', methods=['GET', 'POST'])
+@app.route('/index', methods=['GET', 'POST'])
 def index():
-    return render_template('index.html')
+    if request.method == 'POST':
+        if "view" in request.form.keys():
+            request.form['view']
+        elif "edit" in request.form.keys():
+            return redirect(url_for('edit', post_id=request.form['id']))
+
+    my_posts = db.session.query(Post).all()
+    return render_template('index.html', posts=my_posts)
 
 
-@app.route('/posts/delete/<post_id>', methods=['POST'])
+@app.route('/newpost', methods=['GET', 'POST'])
+def new_post():
+    if request.method == 'POST':
+        title = request.form['title']
+        text = request.form['text']
+
+        today = date.today()
+
+        today = today.strftime("%m-%d-%Y")
+
+        new_post_object = Post(title, text, today)
+
+        db.session.add(new_post_object)
+        db.session.commit()
+
+        return redirect(url_for('index'))
+    else:
+        return render_template('post.html')
+
+
+@app.route('/edit/<post_id>', methods=['GET', 'POST'])
+def edit(post_id):
+    if request.method == 'POST':
+        title = request.form['title']
+        text = request.form['text']
+        post = db.session.query(Post).filter_by(id=post_id).one()
+        post.title = title
+        post.text = text
+        db.session.add(post)
+        db.session.commit()
+        return redirect(url_for('index'))
+    else:
+        my_post = db.session.query(Post).filter_by(id=post_id).one()
+        return render_template('edit.html', post=my_post)
+
+
+@app.route('/header')
+def header():
+    return render_template('header.html')
+
+
+@app.route('/delete/<post_id>', methods=['POST'])
 def delete_post(post_id):
-    """my_post = db.session.query(Post).filter_by(id=post_id).one()
-    db.session.delete(my_post)
-    db.session.commit()"""
+    post = db.session.query(Post).filter_by(id=post_id).one()
+    db.session.delete(post)
+    db.session.commit()
 
-    return render_template('home.html')  # return statement not final
+    return redirect(url_for('index'))
 
 
-app.run(host=os.getenv('IP', '127.0.0.1'), port=int(os.getenv('PORT', 5000)),debug=True)
+app.run(host=os.getenv('IP', '127.0.0.1'), port=int(os.getenv('PORT', 5000)), debug=True)
